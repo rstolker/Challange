fcd1516f36e822831c506e51e2ebee2e307e78c1
rstolker <rstolker@roc-dev.com>
2018-09-28 10:15:24 +0200

    Opdracht 1

---
+++ Index.php
@@ -0,0 +1,53 @@
+<!--
+* naam: Rico Stolker
+* groep:
+* datum: 28-09-2018
+-->
+
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="css/Index.css">
+    <title>Pagina over Rita Hayworth</title>
+</head>
+<body>
+
+<ul>
+    <li><a href="#home">Home</a></li>
+    <li><a href="#news">News</a></li>
+    <li><a href="#contact">Contact</a></li>
+    <li><a href="#about">About</a></li>
+</ul>
+
+<img src="img/rita-hayworth-face-shot.jpeg" alt="Rita_Hayworth" >
+<img src="img/Rita_Hayworth-publicity.jpg" alt="Rita_Hayworth" >
+
+<h1>Rita Hayworth</h1>
+<p>
+    Cras pellentesque iaculis semper. Quisque lobortis urna id ex ullamcorper, imperdiet pharetra turpis posuere. Morbi ornare lectus ac ex aliquet, at mollis metus malesuada. Nunc sodales nulla eleifend accumsan mattis. Nulla et consectetur nunc. Sed congue risus non laoreet ornare. Nam at sollicitudin turpis. Fusce vitae purus est. Mauris aliquet, lorem ac pellentesque eleifend, est lacus sodales metus, quis egestas nisi dolor ut nulla. Quisque pretium scelerisque felis in elementum. Mauris vel ligula ut ante rutrum tristique vitae ut magna. Sed vel ligula consequat, bibendum libero id, euismod turpis. Mauris ac aliquet libero, sit amet mattis lorem. Ut quis odio fermentum, egestas sapien at, rutrum dolor. Donec ac diam tortor. Nam placerat elit ligula, vitae volutpat metus ultricies id.
+</p>
+
+<iframe width="620" height="360" src="https://www.youtube.com/embed/lSdc4kMpWwU">
+</iframe>
+
+<table>
+    <th colspan="3">Filmmografie Als acteur</th>
+    <tr>
+        <td>1972</td>
+        <td>The wrath of God</td>
+        <td>Senore De la Plata</td>
+    </tr>
+    <tr>
+        <td>1971</td>
+        <td>The Naked Zoo</td>
+        <td>Mrs. Golden</td>
+    </tr>
+    <tr>
+        <td>1970</td>
+        <td>Road to Salina</td>
+        <td>Mara</td>
+    </tr>
+</table>
+
+</body>
+</html>
